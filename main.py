765f693abddffd559b4c2fb4d18dd4b61f5e7a81
Luke Yin <lukeyin10@gmail.com>
2025-04-22 15:56:36 +0800

    First Choice

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+'''@@@@ SCSU Jobs Portal @@@@ 
+1. View jobs 
+2. Login 
+0. Exit 
+Enter option: '''
+print("@@@@ SCSU Jobs Portal @@@@")
+print("1. View Jobs")
+print("2. Login")
+print("0. Exit")
+
+choice = input("Enter option: ")
