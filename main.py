4898916ed47931ba58e3071baf01f017ae3eb530
Luke Yin <lukeyin10@gmail.com>
2025-04-24 15:36:41 +0800

    Created a validation check for Initial Display

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ print("1. View Jobs")
 print("2. Login")
 print("0. Exit")
 
-choice = input("Enter option: ")
+
+while True:
+    try:
+        choice = int(input("Enter option: "))
+        if choice < 0 or choice > 2:
+            print("Please enter either 0, 1 or 2.")
+        else:
+            break
+    except:
+        print("Please enter a valid number.")
