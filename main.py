ea816ff6854c89c687dc8aa861bf3379f9eea7d9
Luke Yin <lukeyin10@gmail.com>
2025-04-30 12:28:15 +0800

    Changed Application, Jobseeker, and Job classes to link, added save jobseeker, and finished viewing applicants inside Company Function

--- main.py
+++ main.py
@@ -11,6 +11,56 @@ def check_input(question, lower, upper):
             print("Please enter a valid number.")    
 
 
+class Job:
+    #CompanyTitle, Category, Job Type, Min Education, Exp Required
+    def __init__(self, title, category, job_type, company, min_education, exp_required, tech_skills, mgr_skills, description):  
+            self.title = title
+            self.category = category
+            self.job_type = job_type
+            self.company = company
+            self.min_education = min_education
+            self.exp_required = exp_required
+            self.tech_skills = tech_skills
+            self.mgr_skills = mgr_skills
+            self.description = description.strip()
+            self.applicants = []
+    
+    def add_applicant(self,application):
+        self.applicants.append(application)
+
+class Jobseeker:
+    def __init__(self, username, name, email, education, age, years_experience, tech_skills, mgr_skills, description, applications):
+        self.username = username
+        self.name = name
+        self.email = email
+        self.education = education
+        self.age = age
+        self.years_experience = years_experience
+        self.tech_skills = tech_skills
+        self.mgr_skills = mgr_skills
+        self.description = description
+        if applications:
+            self.applications = applications
+        else:
+            self.applications = []
+
+    #To add the job and application to both jobseeker and job class as a link
+    def apply_for_job(self, job):
+        application = Application(self, job)
+        self.applications.append(application)
+        job.add_applicant(application)
+
+#To use as a linkage between Jobseeker and Jobs
+class Application:
+    def __init__(self, jobseeker, job, company):
+        self.jobseeker = jobseeker
+        self.job = job
+        self.company = company
+        self.status = "Pending"  # Status can be "Pending", "Accepted", or "Rejected"
+
+    def update_status(self, status):
+        self.status = status
+
 #Company class, will be used for two companies, include all information.
 class Company:
     #When user calls the company( ), this will load the company 
@@ -97,13 +147,182 @@ def save_companies(filename, companies):
         
         print("Loading................")
         print("File has been successfully updated.")
-    
+
+def load_jobs(filename):
+    #Create a list jobs to go through
+    jobs = []
+
+    #Take in the user given file name and open the file
+    with open(filename, 'r') as file:
+
+        #Split the file into different blocks/jobs using '-----'
+        blocks = file.read().split('-----')
+
+        #Go through each block
+        for block in blocks:
+
+            #If the block is empty, skip
+            if block.strip() == '':
+                continue
+            
+            #Split the block into two parts, the header line and the description
+            split_block = block.strip().split("\n", 1)
+            header = split_block[0].strip()
+
+            #For descriptions that might be more than 1 line
+            description = "".join(split_block[1:]).strip()
+            
+
+            #If the splitted parts is less than 3, then continue, so no index error will occur when setting variables later
+            parts = header.split(',')
+
+            if len(parts) < 6:
+                continue
+
+            title = parts[0]
+            category = parts[1]
+            job_type = parts[2]
+            company = parts[3]
+            min_education = parts[4]
+            exp_required = parts[5]
+
+            #Append it into a list while creating it as a class Job( )
+            jobs.append(Job(title.strip(), category.strip(), job_type.strip(), company.strip(), min_education.strip(), exp_required.strip(), [], [], description.strip()))
+
+    #Finally, return the jobs array
+    return jobs
+
+def load_jobseekers(filename):
+    #Create a list jobseekers to go through
+    jobseekers = []
+
+    #Take in the user given file name and open the file
+    with open(filename, 'r') as file:
+
+        #Split the file into different blocks/jobseekers using '-----'
+        blocks = file.read().split('-----')
+
+        #Go through each block
+        for block in blocks:
+
+            #If the block is empty, skip
+            if block.strip() == '':
+                continue
+            
+            #Split the block into different lines
+            lines = block.strip().splitlines()
+            if len(lines) < 1:
+                continue
+            
+            #First, the header
+            header = lines[0].strip().split(',')
+            if len(header) < 6:
+                continue
+
+            username = header[0].strip()
+            name = header[1].strip()
+            email = header[2].strip()
+            education = header[3].strip()
+            age = header[4].strip()
+            years_experience = header[5].strip()
+            
+            #Then, empty strings to store the values
+            tech_skills = ""
+            mgr_skills = ""
+
+            #Only if the number of values is greater than 1, split
+            if len(lines) > 1:
+                skills = lines[1].strip().split(',')
+                if len(skills) > 0:
+                    tech_skills = skills[0].strip()
+                if len(skills) > 1:
+                    mgr_skills = skills[1].strip()
+             
+            #Since, description is stored on third line, only if length of the lines are greater than 3, add description
+            description = ""
+            if len(lines) > 2:
+                description = lines[2].strip()
+            
+            #List to store the applications
+            apps = []
+            #only after description
+            for i in range(3, len(lines)):
+
+                #If the line starts with "Application"
+                line = lines[i].strip()
+                if line.startswith("Application"):
+                    app_info = line.split(":")[1].strip()
+                    parts = app_info.split(",")
+                    if len(parts) >= 3:
+                        job_title = parts[0].strip()
+                        job_company = parts[1].strip()
+                        apps.append(Application(username, job_title, job_company))
+            
+            jobseeker = Jobseeker(username, name, email, education, age, years_experience, 
+                                tech_skills, mgr_skills, description, apps)
+            jobseekers.append(jobseeker)
+
+    #Finally, return the jobseekers array
+    return jobseekers
+
+def link_applications(jobseekers, jobs):
+    #Go through jobseeker
+    for jobseeker in jobseekers:
+        #Create a copy since we dont want to change the list directly to not have errors
+        copy = jobseeker.applications
+        for application in copy:
+            # Find the job by title and company
+            for job in jobs:
+                if job.title.strip() == application.job.strip() and job.company.strip() == application.company.strip():
+                    application.job = job
+                    application.jobseeker = jobseeker
+                    job.add_applicant(application)
+
+                    #Replace the first placeholder since it doesnt include proper Job and Jobseeker Object
+                    jobseeker.applications.pop(0) 
+
+
+def save_jobs(filename, jobs):
+    #Open the file and rewrite
+    with open(filename, 'w') as file:
+        #For every job, write into the original format, with ----- in between each job
+        for job in jobs:
+            file.write(f"{job.title},{job.category},{job.job_type},{job.company},{job.min_education},{job.exp_required}\n")
+            file.write(f"{job.description.strip()}\n")
+            file.write("-----\n")
+        
+        print("Loading................")
+        print("File has been successfully updated.")
+        print()
+
+def save_jobseekers(filename, jobseekers):
+    #Open the file and rewrite
+    with open(filename, 'w') as file:
+        #For every jobseeker, write into the original format, with ----- in between each jobseeker
+        for jobseeker in jobseekers:
+            file.write(f"{jobseeker.username},{jobseeker.name},{jobseeker.email},{jobseeker.education},{jobseeker.age},{jobseeker.years_experience}\n")
+            file.write(f"{jobseeker.tech_skills.strip()},{jobseeker.mgr_skills.strip()}\n")
+            file.write(f"{jobseeker.description.strip()}\n")
+            for application in jobseeker.applications:
+                file.write("Application: ")
+                file.write(f"{application.jobseeker},{application.job.title},{application.status}\n")
+            file.write("-----\n")
+        
+        print("Loading................")
+        print("File has been successfully updated.")
+
 #Take in which company we are gonna use, according to the log in
 def company(company):
     
     while True:
         #Before starting, load the companies into an array
         companies = load_companies("companyinfo.txt")
+        jobs = load_jobs("jobs.txt") 
+        jobseekers = load_jobseekers("jobseeker.txt")
+        
+        link_applications(jobseekers, jobs)
+        
+        index = find_company(company, companies)
 
         #Ask the users for the first option
         print(" 1) Edit Company Profile")
@@ -115,8 +334,6 @@ def company(company):
 
         #If option is 1, then, show the currennt company info
         if option == 1:
-            
-            index = find_company(company, companies)
 
             if index == -1:
                 print("The company does not exist.")
@@ -143,6 +360,40 @@ def company(company):
             
             elif answer == 0:
                 continue
+        
+        if option == 2:
+            #If option is 2, then show the jobs that the company has posted
+            print("Jobs posted by the company:")
+            i = 1
+            job_index = {}
+            for job in jobs:
+                if job.company == companies[index].username:
+                    print(f"{index}) Title: {job.title}, Category: {job.category}, Job Type: {job.job_type}, Min Education: {job.min_education}, Exp Required: {job.exp_required}")
+                    print(f"Description: {job.description}")
+                    job_index[i] = job
+                    i+=1
+            
+            if len(job_index) == 0:
+                print("No jobs posted by the company.")
+                continue
+            else:
+                option = check_input("Enter the number of job to view in detail, or 0 to exit", 1,i)
+
+                if option == 0:
+                    continue
+                else:
+
+                    #Get the selected job from the dictionary
+                    selected_job = job_index[option]
+                    #Get how many applicants there are
+                    print(f"There are {len(selected_job.applicants)} applicants for this job.")
+                    option = check_input("Enter 1 to view applicants, or 0 to exit",0,1)
+
+                    if option == 1:
+                        print(f"{"":<4} {"Name":<20} {"Age":<15} {"Education":<20} {"Years Experience":<25}")
+                        index = 1
+                        for applicant in selected_job.applicants:
+                            print(f"{index}{")":<3} {applicant.jobseeker.name :<20} {applicant.jobseeker.age :<15} {applicant.jobseeker.education:<20} {applicant.jobseeker.years_experience:<25}")
 
         elif option == 4:
             break
